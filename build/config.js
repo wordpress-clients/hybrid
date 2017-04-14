7d1b9a4f74689ae943b25ce5ad656d95009e9235
shprink <julien.renaux@gmail.com>
2017-04-14 09:56:08 +0200

    fix translation

--- build/config.js
+++ build/config.js
@@ -14,13 +14,14 @@ module.exports = {
   entry: process.env.IONIC_APP_ENTRY_POINT,
   output: {
     path: '{{BUILD}}',
+    publicPath: 'build/',
     filename: process.env.IONIC_OUTPUT_JS_FILE_NAME,
     devtoolModuleFilenameTemplate: ionicWebpackFactory.getSourceMapperFunction(),
   },
   devtool: process.env.IONIC_SOURCE_MAP_TYPE,
 
   resolve: {
-    extensions: ['.ts', '.js', '.json'],
+    extensions: ['.ts', '.js', '.json', '.cson'],
     modules: [path.resolve('node_modules')]
   },
 
@@ -28,7 +29,8 @@ module.exports = {
     rules: [
       {
         test: /\.json$/,
-        use: 'json-loader'
+        use: 'json-loader',
+        exclude: path.join(__dirname, '..', 'src', 'i18n')
       }, {
         test: /service-worker\.js$/,
         use: [
@@ -38,8 +40,11 @@ module.exports = {
         include: path.join(__dirname, '..', 'src')
       },
       {
-        test: /\.cson$/,
-        use: ['file-loader?name=/i18n/[name].json', 'strip-module-export-loader', 'cson-loader'],
+        test: /\.json$/,
+        use: [
+          'file-loader?name=[name].[ext]',
+          'json-loader'
+        ],
         include: path.join(__dirname, '..', 'src', 'i18n')
       },
       {
--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,4 +1,18 @@
+// import './en.cson';
+
 function requireAll(requireContext) {
-  return requireContext.keys().map(requireContext);
+
+  return requireContext.keys().forEach((test) => {
+    console.log('test', test)
+  });
 }
-requireAll(require.context("./", true, /^\.\/.*\.cson$/));
+  try {
+    requireAll(require.context("./", true, /^\.\/.*\.json$/));
+  } catch (error) {
+    console.error('requireAll', error)
+  }
+
+
+// console.log('test', require.context("./", true, /^\.\/.*\.json$/))
+// import '!file-loader?name=/i18n/[name].json!./en.cson';
+

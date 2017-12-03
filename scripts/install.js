b1f2208023791d7195ac337a104c36e0ee1ec7c8
shprink <julien.renaux@gmail.com>
2017-12-03 12:52:45 +0100

    forces config overwrite when CI

--- scripts/install.js
+++ scripts/install.js
@@ -12,11 +12,12 @@ copy('../dist/root/ionic.config.json', '../ionic.config.json');
 copy('../release.sh.dist', '../release.sh');
 
 function copy(source, target, overwrite) {
+    overwrite = process.env.CI ? true : overwrite;
     if (!overwrite && fs.existsSync(target)) {
         console.log('the destination already exist, will not overwrite: ' + target);
         return;
     }
-    fs.copy(source, target, function(err) {
+    fs.copy(source, target, function (err) {
         if (err) return console.error(err)
         console.log('success: ' + target)
     })
